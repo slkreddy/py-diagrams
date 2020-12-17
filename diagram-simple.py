2347fdc4a87534c04bd4a22538dedba79dadfe15
LaxmiKumar Reddy Sammeta <slkreddysite@gmail.com>
2020-12-17 20:40:06 +0530

    Create diagram-simple.py

---
+++ diagram-simple.py
@@ -0,0 +1,11 @@
+# diagram-simple.py
+from diagrams import Diagram
+from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
+from diagrams.aws.network import ELB
+
+from diagrams.k8s.compute import Pod, StatefulSet
+
+
+with Diagram("Web Service", show=False):
+ (Pod("GUI") >>  ELB("lb")) >> EC2("web") >> RDS("userdb")
