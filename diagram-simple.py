78d2be54c339d671410e527617d747bfe0e4e58b
LaxmiKumar Reddy Sammeta <slkreddysite@gmail.com>
2020-12-17 20:51:57 +0530

    Update diagram-simple.py

--- diagram-simple.py
+++ diagram-simple.py
@@ -7,5 +7,5 @@ from diagrams.aws.network import ELB
 from diagrams.k8s.compute import Pod, StatefulSet
 
 
-with Diagram("Web Service", show=False):
+with Diagram("diagram-simple", show=False):
  (Pod("GUI") >>  ELB("lb")) >> EC2("web") >> RDS("userdb")
